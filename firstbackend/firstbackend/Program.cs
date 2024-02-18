cb5479f864903db3fa0f965ed5d4396c2414324e
Heyder <guliyevheydar0@gmail.com>
2024-02-18 19:55:27 +0400

    yuklendi

---
+++ firstbackend/firstbackend/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace firstbackend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testHello = ("Hello World And Guys");
+            char x = 'A';
+            float testFloat = 1 / 3F;
+            double testDouble = 1 / 3D;
+decimal testDecimal = 1 / 3M;
+
+            Console.WriteLine(testFloat);
+            Console.WriteLine(testDouble);
+            Console.WriteLine(testDecimal);
+
+            byte ageByte = +15;
+            int ageInt = -15;
+            long ageLong = -15;
+
+            Console.WriteLine(ageByte);
+            Console.WriteLine(ageInt);
+            Console.WriteLine(ageLong);
+
+
+            Console.WriteLine(testHello);
+            Console.WriteLine(testHello);
+
+        }
+    }
+}
---
+++ lesson 1/calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please, add first number:");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please, add Second number:");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add Symbol");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            //int result = x1 + x2;
+            //Console.WriteLine(result + " Result");
+        //    if (result > 0 && result < 1000)
+        //    {
+        //        Console.WriteLine("The sum is  than smaller 500");
+        //    }
+        //    else if (result > 50 && result < 100)
+        //    {
+        //        Console.WriteLine("The sum is smaller than 1000");
+        //    }
+        //    else { 
+        //    Console.WriteLine("The sum is greater than 1000");
+        //}
+        if (symbol == '+')
+            {
+                Console.WriteLine((x1 + x2) + " result");
+            }
+        else if(symbol== '-')
+            {
+                Console.WriteLine((x1 - x2) + " result");
+            }
+        else if(symbol== '/') 
+            {
+            Console.WriteLine((x1 / x2) + " Result");
+            }
+            else if (symbol == '*')
+            {
+                Console.WriteLine((x1 * x2) + " Result");
+            }
+
+
+
+
+        }
+    }
+}
