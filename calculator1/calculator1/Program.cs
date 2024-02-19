bad717f933000ffeebf6fa4d12f65fad9eec093c
Heyder <guliyevheydar0@gmail.com>
2024-02-19 14:37:03 +0400

    download

---
+++ calculator1/calculator1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace calculator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("please, add first number:");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            if (x1 < 0)
+            {
+                Console.Write("please, add first number:");
+               x1 = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("please, add Second number:");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            if (x2 < 0)
+            {
+                Console.Write("please, add first number:");
+                x1 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Add Symbol");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+
+
+            //int result = x1 + x2;
+            //Console.WriteLine(result + " Result");
+            //    if (result > 0 && result < 1000)
+            //    {
+            //        Console.WriteLine("The sum is  than smaller 500");
+            //    }
+            //    else if (result > 50 && result < 100)
+            //    {
+            //        Console.WriteLine("The sum is smaller than 1000");
+            //    }
+            //    else { 
+            //    Console.WriteLine("The sum is greater than 1000");
+            //}
+
+
+            if (symbol == '+')
+            {
+                Console.WriteLine((x1 + x2) + " result");
+            }
+            else if (symbol == '-')
+            {
+                Console.WriteLine((x1 - x2) + " result");
+            }
+            else if (symbol == '/')
+            {
+                Console.WriteLine((x1 / x2) + " Result");
+            }
+            else if (symbol == '*')
+            {
+                Console.WriteLine((x1 * x2) + " Result");
+            }
+
+
+        }
+    }
+}
